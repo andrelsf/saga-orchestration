f033a8e0ff757cd0e97b5f669df4ee0f9b16f3fa
Andre Ferreira <andre.souza@sensedia.com>
2022-04-08 18:01:00 -0300

    Added init-envorionment.sh

--- init-environment.sh
+++ init-environment.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 #
 # Author: Andre Ferreira <andre.dev.linux@gmail.com>
-# Date..:   
+# Date..: 08/04/2022  
 #
+
+PROJECTS=$(ls . | grep mc-)
+
+for PROJECT in $PROJECTS
+do
+  if [ -d "$PROJECT" ]; then
+    echo "Cloning project..."
+    git clone https://github.com/andrelsf/"$PROJECT".git
+  fi
+done
+
+docker-compose up -d
