86c7ddab860b3d207663a6052d8922317709fea0
Andre Ferreira <andre.souza@sensedia.com>
2022-04-10 09:48:26 -0300

    Automate envoronment

--- init-environment.sh
+++ init-environment.sh
@@ -3,6 +3,15 @@
 # Author: Andre Ferreira <andre.dev.linux@gmail.com>
 # Date..: 08/04/2022  
 #
+# necessary tools:
+#   - Java 11+: (Install) 
+#       sdkman -> https://sdkman.io/install
+#   - maven: 
+#       (Install) -> https://maven.apache.org/install.html
+#   - docker: 
+#       (Install) -> https://docs.docker.com/engine/install/
+#   - docker-compose: 
+#       (Install) ->  https://docs.docker.com/compose/install/
 
 PROJECTS=$(ls . | grep mc-)
 
@@ -11,6 +20,7 @@ do
   if [ -d "$PROJECT" ]; then
     echo "Cloning project..."
     git clone https://github.com/andrelsf/"$PROJECT".git
+    cd $PROJECT && mvn clean install package && cd .. 
   fi
 done
 
